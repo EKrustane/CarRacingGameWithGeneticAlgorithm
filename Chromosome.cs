aadf51ffbacbb9346c334fcb79e351e1a7d5e84f
Ermīne Krustāne <erminekrustane@inbox.lv>
2023-04-08 22:12:08 +0300

    Izveidota indivīdu klase, kurā saglabājas indivīdu piemērotība. 8.04.23. 20:00.

---
+++ Chromosome.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRacingGameWithGeneticAlgorithm
+{
+    public class Chromosome
+    { 
+        public int Fitness { get; set; } = 0;
+
+        public Chromosome()
+        {
+            InitializeChromosome();
+        }
+
+        private void InitializeChromosome()
+        {
+            
+        }
+    }
+}
